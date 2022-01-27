c6f2534e34285a8e4e3a35783f9de815679d103a
antonkalmbah <anton.kalmbah@yandex.ru>
2022-01-27 22:33:33 +0300

    Исправил ошибку с импортом
    открывается чат, ошибки при создании комнаты нет

--- moduleE6_homework_6.9/chat/chat/asgi.py
+++ moduleE6_homework_6.9/chat/chat/asgi.py
@@ -2,7 +2,7 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import chat_app
+import chat_app.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
 
