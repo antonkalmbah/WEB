16d6e07310db8940c06284c163df4634c35a6fa7
antonkalmbah <anton.kalmbah@yandex.ru>
2022-03-20 13:21:13 +0300

    npm_project ошибки в путях с файлами index.html и проблемы с loader

--- project_npm/webpack.config.js
+++ project_npm/webpack.config.js
@@ -1,6 +1,36 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: './src/index.js', // абсолютный путь, файл начала, нужен обязательно
     output: {
         filename: 'main.js'
-    }
-}
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.pug',
+            filename: 'index.html',
+        }),
+        new MiniCssExtractPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    // option: {
+                    //     esModule: true,
+                    // },
+                }, 'css-loader'],
+                test: /\.css&/
+            },
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+                // option: {
+                //     pretty: true
+                // }
+            }
+        ]
+    },
+};
