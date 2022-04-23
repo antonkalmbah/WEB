35b6514a5a045866b3890caf895ba67cfaac211b
antonkalmbah <anton.kalmbah@yandex.ru>
2022-04-23 10:45:18 +0300

    Yandex API не работают

---
+++ projects/project_react_weather/my_weather_app/src/Weather.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { YMaps, Map } from 'react-yandex-maps';
+
+const Weather = () => (
+  <YMaps>
+    <div>
+      My awesome application with maps!
+      <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
+    </div>
+  </YMaps>
+);
+
+export default Weather;
